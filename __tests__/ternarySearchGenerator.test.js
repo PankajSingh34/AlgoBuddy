dd8e6daefcc39b32dca5e0cc006e5e07264e0d32
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-07-06 15:18:55 +0530

    test: add unit tests for features/algorithms/array/ternarySearchLogic.js (#2882)

---
+++ __tests__/ternarySearchGenerator.test.js
@@ -0,0 +1,6 @@
+const module = require("features/algorithms/array/ternarySearchLogic.js");
+describe("ternarySearchGenerator", () => {
+  test("exports expected API", () => {
+    expect(module).toBeDefined();
+  });
+});
